24b2585a85a4f0bdd56343d27a0d4332b7acd614
Oluwatowo Rosanwo <olueatoworosanwo@gmail.com>
2023-08-14 09:27:28 +0100

    added smooth scroller, done hero and two sections

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 import "./style.css";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from "gsap";
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+let smoother = ScrollSmoother.create({
+    smooth: 1.5,
+    effects: true,
+    normalizeScroll: true,
+    wrapper: "#smooth-wrapper",
+    content: "#smooth-content",
+});
+
+console.log(smoother);
+
+// HTML Divs Loaded
+window.addEventListener("DOMContentLoaded", (event) => {
+    ScrollTrigger.refresh();
+});
+// All images and assets loaded
+document.addEventListener("load", function () {
+    ScrollTrigger.refresh();
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["index.html"],
+    theme: {
+        extend: {
+            colors: {
+                neutral: {
+                    light: "#D0D5DD",
+                },
+            },
+            container: {
+                center: true,
+                padding: {
+                    DEFUALT: "20px",
+                },
+            },
+            fontFamily: {
+                sans: ["Open Sans", "ssans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
