97da6860e0ae56a9491e4d13cfcd61a680b7b6d4
Oluwatowo Rosanwo <olueatoworosanwo@gmail.com>
2023-08-15 16:55:05 +0100

    made full page animation

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ import "swiper/css/navigation";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
+let pageloaded = false;
+
 let smoother = ScrollSmoother.create({
     smooth: 1.5,
     effects: true,
@@ -70,8 +72,498 @@ function swiperInit() {
     };
 }
 
+function splitText() {
+    const allWords = [];
+    const allLines = [];
+    const splitWordText = document.querySelectorAll(".split-word");
+    const splitLineText = document.querySelectorAll(".split-line");
+
+    splitWordText.forEach((text) => {
+        const words = new SplitText(text, {
+            type: "words",
+            wordsClass: "word",
+        });
+
+        allWords.push(...words.words);
+    });
+
+    splitLineText.forEach((text) => {
+        const lines = new SplitText(text, {
+            type: "lines",
+            linesClass: "line",
+        });
+
+        allLines.push(...lines.lines);
+    });
+
+    gsap.set(allWords, { y: "100%", opacity: 0 });
+    gsap.set(allLines, { y: "100%", opacity: 0, scale: 0.8 });
+}
+
+function positionElements() {
+    gsap.set("header>div", { y: "-200%", opacity: 0 });
+    gsap.set("#alert > .container > div > div > img:nth-child(1)", {
+        x: "-200%",
+        opacity: 0,
+        scale: 1.2,
+    });
+    gsap.set("#alert > .container > div > div > img:nth-child(2)", { y: -164 });
+    gsap.set("#hero button", { opacity: 0, y: "100%", ease: "power3.out", duration: 2 }, "-=1");
+    gsap.set(
+        "#hero button + img",
+        { opacity: 0, y: "100%", ease: "power3.out", duration: 2 },
+        "-=1.85"
+    );
+}
+
 addEventListener("DOMContentLoaded", marquee);
 
 addEventListener("DOMContentLoaded", backToTop);
 
 addEventListener("DOMContentLoaded", swiperInit);
+
+addEventListener("DOMContentLoaded", splitText);
+
+addEventListener("DOMContentLoaded", positionElements);
+
+addEventListener("load", () => {
+    pageloaded = true;
+});
+
+function loader() {
+    const loader = document.getElementById("loader");
+    const svg = loader.querySelector("svg");
+    const cover = loader.querySelector("#cover");
+
+    const topR = svg.children[0];
+    const bottomR = svg.children[1];
+    const text = svg.children[2];
+
+    gsap.set(topR, { rotate: 45, opacity: 0 });
+    gsap.set(bottomR, { rotate: 45, opacity: 0 });
+    gsap.set(text, { x: "-100%", opacity: 0 });
+    gsap.set(cover, { scaleX: 0, transformOrigin: "right" });
+
+    const logoTimeline = gsap.timeline();
+    const coverTimeline = gsap.timeline({
+        repeat: -1,
+        yoyo: false,
+        repeatDelay: 0.75,
+        delay: 3,
+        defaults: {
+            delay: 0.75,
+            duration: 0.75,
+            ease: "sine.out",
+        },
+    });
+
+    logoTimeline
+        .to(topR, { rotate: 0, opacity: 1, delay: 1 })
+        .to(bottomR, { rotate: 0, opacity: 1 })
+        .to(text, {
+            x: 0,
+            opacity: 1,
+            ease: "expo.out",
+            duration: 1.5,
+        });
+
+    coverTimeline
+        .to(cover, { scaleX: 1, delay: 0 })
+        .to(cover, { scaleX: 0, delay: 0, transformOrigin: "left" })
+        .to(cover, { scaleX: 1, delay: 1, delay: 0.75 })
+        .to(cover, {
+            scaleX: 0,
+            delay: 0,
+            transformOrigin: "right",
+            onComplete() {
+                if (pageloaded) {
+                    coverTimeline.kill();
+                    gsap.to(loader, { y: "-100%", onComplete: main });
+                }
+            },
+        });
+}
+
+loader();
+
+function main() {
+    function headerAnimation() {
+        gsap.to("header>div", { y: 0, opacity: 1, ease: "expo.out", duration: 2 });
+    }
+
+    function heroAnimation() {
+        let timeline = gsap.timeline({ delay: 1 });
+        timeline
+            .to("#hero h1 .word", {
+                y: 0,
+                opacity: 1,
+                stagger: { each: 0.05, from: "start" },
+                ease: "power3.out",
+                duration: 0.8,
+            })
+            .to(
+                "#hero p .line",
+                {
+                    opacity: 1,
+                    scale: 1,
+                    y: 0,
+                    ease: "power3.out",
+                    duration: 1,
+                    stagger: { each: 0.1, from: "start" },
+                },
+                "-=0.5"
+            )
+            .to("#hero button", { opacity: 1, y: 0, ease: "power3.out", duration: 2 }, "-=1")
+            .to(
+                "#hero button + img",
+                { opacity: 1, y: 0, ease: "power3.out", duration: 2 },
+                "-=1.85"
+            );
+    }
+
+    function future() {
+        const timeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#future",
+                start: "top center",
+            },
+        });
+
+        timeline
+            .to("#future h2 .line", {
+                y: 0,
+                opacity: 1,
+                scale: 1,
+                duration: 2,
+                ease: "power3.out",
+                stagger: { each: 0.125 },
+            })
+            .from(
+                "#future h2 + img",
+                {
+                    opacity: 0,
+                    y: "100%",
+                    duration: 2,
+                    ease: "expo.out",
+                },
+                "-=1.5"
+            );
+    }
+
+    function steps() {
+        const timeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#steps ul",
+                start: "top center",
+            },
+        });
+
+        timeline.from("#steps ul li", {
+            scale: 0.8,
+            opacity: 0,
+            stagger: 0.2,
+            opacity: 0,
+            duration: 1.5,
+            ease: "back.out(1.7)",
+        });
+    }
+
+    function getStarted() {
+        const timeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#get-started",
+                start: "top center",
+            },
+        });
+
+        timeline
+            .to("#get-started h2 .word", {
+                y: 0,
+                opacity: 1,
+                stagger: { each: 0.05, from: "start" },
+                ease: "power3.out",
+                duration: 0.8,
+            })
+            .to(
+                "#get-started p .line",
+                {
+                    opacity: 1,
+                    scale: 1,
+                    y: 0,
+                    ease: "power3.out",
+                    duration: 1,
+                    stagger: { each: 0.1, from: "start" },
+                },
+                "-=0.5"
+            )
+            .from(
+                "#get-started button",
+                { opacity: 0, y: "100%", ease: "power3.out", duration: 2 },
+                "-=1"
+            )
+            .from(
+                "#get-started img",
+                {
+                    y: "100%",
+                    ease: "expo.out",
+                    duration: 2,
+                },
+                "-=2.25"
+            );
+    }
+
+    function toggle() {
+        const textTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#toggle > .container > div > article",
+                start: "top 75%",
+            },
+        });
+
+        const toggleTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#toggle > .container > div > div",
+                start: "top 75%",
+            },
+        });
+
+        textTimeline
+            .to("#toggle > .container > div > article h2 .word", {
+                y: 0,
+                opacity: 1,
+                scale: 1,
+                duration: 0.8,
+                ease: "power3.out",
+                stagger: { each: 0.05 },
+            })
+            .to(
+                "#toggle > .container > div > article > p .line",
+                {
+                    opacity: 1,
+                    scale: 1,
+                    y: 0,
+                    ease: "power3.out",
+                    duration: 1,
+                    stagger: { each: 0.1, from: "start" },
+                },
+                "-=1"
+            );
+
+        toggleTimeline.from("#toggle > .container > div > div > article", {
+            opacity: 0,
+            y: "200%",
+            ease: "expo.out",
+            duration: 2,
+            stagger: 0.25,
+            delay: 1,
+        });
+    }
+
+    function ourApp() {
+        gsap.to("#our-app h2 .word", {
+            y: 0,
+            opacity: 1,
+            stagger: { each: 0.05, from: "start" },
+            ease: "power3.out",
+            duration: 0.8,
+            scrollTrigger: {
+                trigger: "#our-app",
+                start: "top center",
+            },
+        });
+
+        gsap.from("#our-app > .container > div > div > img", {
+            y: "300%",
+            opacity: 1,
+            ease: "expo.out",
+            duration: 2,
+            scale: 0.8,
+            scrollTrigger: {
+                trigger: "#our-app > .container",
+                start: "center center",
+            },
+        });
+
+        const articles = document.querySelectorAll("#our-app article");
+
+        articles.forEach((article) => {
+            const timeline = gsap.timeline({
+                scrollTrigger: {
+                    trigger: article,
+                    start: "top 75%",
+                },
+            });
+
+            timeline.from(article.children, {
+                opacity: 0,
+                y: "300%",
+                stagger: 0.25,
+                ease: "power3.out",
+            });
+        });
+    }
+
+    function alert() {
+        const alertTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#alert > .container > div > div",
+                start: "top 75%",
+            },
+        });
+
+        alertTimeline
+            .to("#alert > .container > div > div > img:nth-child(2)", {
+                delay: 1,
+                y: 0,
+                duration: 1,
+                ease: "back.out(1.5)",
+            })
+            .to(
+                "#alert > .container > div > div > img:nth-child(1)",
+                {
+                    x: 0,
+                    opacity: 1,
+                    duration: 1,
+                    ease: "expo.out",
+                },
+                "-=1"
+            )
+            .to(
+                "#alert > .container > div > div > img:nth-child(1)",
+                {
+                    scale: 1,
+                    duration: 0.5,
+                },
+                "-=0.25"
+            )
+            .to(
+                "#alert > .container > div h2 .line",
+                {
+                    opacity: 1,
+                    scale: 1,
+                    y: 0,
+                    ease: "power3.out",
+                    duration: 1,
+                    stagger: { each: 0.1, from: "start" },
+                },
+                "-=3"
+            );
+    }
+
+    function testimonial() {
+        gsap.to("#testimonial h2 .word", {
+            y: 0,
+            opacity: 1,
+            stagger: { each: 0.05, from: "start" },
+            ease: "power3.out",
+            duration: 0.8,
+            scrollTrigger: {
+                trigger: "#testimonial h2",
+                start: "top center",
+            },
+        });
+    }
+
+    function spotify() {
+        gsap.from("#spotify > div > img:nth-child(1)", {
+            opacity: 0,
+            x: "-100%",
+            ease: "expo.out",
+            duration: 2,
+            scrollTrigger: {
+                trigger: "#spotify > div > img:nth-child(1)",
+                start: "top 75%",
+            },
+        });
+
+        gsap.from("#spotify > div > img:nth-of-type(2)", {
+            opacity: 0,
+            x: "100%",
+            duration: 2,
+            ease: "expo.out",
+            scrollTrigger: {
+                trigger: "#spotify > div > img:nth-of-type(2)",
+                start: "top 75%",
+            },
+        });
+
+        const contenTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#spotify > div",
+                start: "top 75%",
+            },
+        });
+
+        contenTimeline
+            .to("#spotify > div > div > h2 .word", {
+                y: 0,
+                opacity: 1,
+                stagger: { each: 0.05, from: "start" },
+                ease: "power3.out",
+                duration: 0.8,
+            })
+            .to(
+                "#spotify > div > div > p .line",
+                {
+                    opacity: 1,
+                    scale: 1,
+                    y: 0,
+                    ease: "power3.out",
+                    duration: 1,
+                    stagger: { each: 0.1, from: "start" },
+                },
+                "-=0.5"
+            )
+            .from("#spotify > div > div > p + div > *", {
+                y: "100%",
+                opacity: 0,
+                stagger: 0.25,
+            });
+    }
+
+    function waitlist() {
+        const timeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#waitlist",
+                start: "top 75%",
+            },
+        });
+
+        timeline
+            .from("#waitlist > div  h2", {
+                y: "100%",
+                opacity: 0,
+                duration: 1.5,
+                ease: "expo.out",
+            })
+            .to("#waitlist > div  h3 .word", {
+                y: 0,
+                opacity: 1,
+                stagger: { each: 0.05, from: "start" },
+                ease: "power3.out",
+                duration: 0.8,
+            })
+            .from(
+                "#waitlist > div  button",
+                {
+                    y: "100%",
+                    opacity: 0,
+                    duration: 1.5,
+                    ease: "expo.out",
+                },
+                "-=0.75"
+            );
+    }
+
+    headerAnimation();
+    heroAnimation();
+    future();
+    steps();
+    getStarted();
+    toggle();
+    ourApp();
+    alert();
+    testimonial();
+    spotify();
+    waitlist();
+}
