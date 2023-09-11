299b2dbc4f21d0e22d9a1f498b639db8d7c691f0
Oluwatowo Rosanwo <olueatoworosanwo@gmail.com>
2023-09-11 13:35:33 +0100

    moved firebase config to env

--- firebase.js
+++ firebase.js
@@ -1,20 +1,30 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { addDoc, collection, getFirestore, serverTimestamp } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+const {
+    VITE_API_KEY,
+    VITE_AUTH_DOMAIN,
+    VITE_PROJECT_ID,
+    VITE_STORAGE_BUCKET,
+    VITE_MESSAGING_SENDER_ID,
+    VITE_APP_ID,
+    VITE_MEASUREMENT_ID,
+} = import.meta.env;
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCZijOT51EWxaSD6wfni7iG33OvoDhltbE",
-    authDomain: "rely-3d4d0.firebaseapp.com",
-    projectId: "rely-3d4d0",
-    storageBucket: "rely-3d4d0.appspot.com",
-    messagingSenderId: "796944035349",
-    appId: "1:796944035349:web:f2f066c8817f8cef48281c",
-    measurementId: "G-ZHKKCD0THX",
+    apiKey: VITE_API_KEY,
+    authDomain: VITE_AUTH_DOMAIN,
+    projectId: VITE_PROJECT_ID,
+    storageBucket: VITE_STORAGE_BUCKET,
+    messagingSenderId: VITE_MESSAGING_SENDER_ID,
+    appId: VITE_APP_ID,
+    measurementId: VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
